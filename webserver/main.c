579ac1228acf7c976468a04f523418c02625aca4
Alexandre DUHAMEL <duhamela@bouleau16.iut-infobio.priv.univ-lille1.fr>
2019-02-08 16:25:02 +0100

    Perroquet qui répond

--- webserver/main.c
+++ webserver/main.c
@@ -3,18 +3,31 @@
 
 #include "socket.h"
 
+#define BUFFER_LEN 10
+
 int main (/*int argc , char **argv*/ ){
 
     /* creation des sockets server et client */
     int socket_server = creer_serveur(8080);
-    int socket_client = accept (socket_server, NULL, NULL);
 
-    if (socket_client == -1) {
-      perror ("accept");
-      /* traitement d ’ erreur */
+    while(1){
+      int socket_client = accept (socket_server, NULL, NULL);
+
+      if (socket_client == -1) {
+        perror ("accept");
+        /* traitement d ’ erreur */
+      }
+      /* On peut maintenant dialoguer avec le client */
+
+      const char * message_bienvenue = "Bonjour, bienvenue sur mon serveur\n" ;
+
+      write(socket_client, message_bienvenue, strlen(message_bienvenue));
+
+      char buffer[BUFFER_LEN];
+      int ret;
+      while( (ret = read(socket_client, buffer, BUFFER_LEN)) != -1 ){
+        write(socket_client, buffer, ret);
+        memset(buffer, 0, BUFFER_LEN);
+      }
     }
-    /* On peut maintenant dialoguer avec le client */
-    const char * message_bienvenue = "Bonjour, bienvenue sur mon serveur\n" ;
-    write(socket_client, message_bienvenue, strlen(message_bienvenue));
-
-  }
+}
