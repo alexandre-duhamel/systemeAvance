f992efbf9f735afc9b39a9a15dd3a3b800bcb3d8
Mickael Spilmont <mickael.spilmont.fr@gmail.com>
2019-02-08 12:11:10 +0100

    Creation du serveur terminée mais non testé

--- webserver/socket.c
+++ webserver/socket.c
@@ -1,2 +1,32 @@
-#include <sys/net.h>
+#include <stdio.h>
 #include <sys/socket.h>
+#include <netinet/ip.h>
+
+int creer_serveur(int port) {
+    int socket_server;
+    struct sockaddr_in socketAddr;
+
+    // paramètrage de la structure socketAddr
+    socketAddr.sin_family = AF_INET;
+    socketAddr.sin_port = htons (8080);
+    socketAddr.sin_addr.s_addr = INADDR_ANY;
+
+    // création de la socket serveur 
+    socket_server = socket(AF_INET, SOCK_STREAM, 0);
+    // Vérification du bon déroullement de sa création
+    if (socket_server == -1) {
+        perror("socket_serveur");
+    }
+
+    // Attachement de la socket à une interface réseau et paramètrage
+    if (bind(socket_server, (struct sockaddr *)&socketAddr, sizeof(socketAddr)) == -1) {
+        perror("bind socket_serveur");
+    }
+
+    // démarrer l'attente de connexions
+    if (listen(socket_server, 10) == -1) {
+        perror("listen socket_serveur");
+    }
+}
+
+
