26dd92cca3fa4b9f150cdf1260f8f6edb703af9c
fretell <fretelliza@gmail.com>
2019-02-08 14:12:51 +0100

    socket serveur qui compile

--- webserver/socket.c
+++ webserver/socket.c
@@ -8,7 +8,7 @@ int creer_serveur(int port) {
 
     // paramètrage de la structure socketAddr
     socketAddr.sin_family = AF_INET;
-    socketAddr.sin_port = htons (8080);
+    socketAddr.sin_port = htons (port);
     socketAddr.sin_addr.s_addr = INADDR_ANY;
 
     // création de la socket serveur 
@@ -16,17 +16,19 @@ int creer_serveur(int port) {
     // Vérification du bon déroullement de sa création
     if (socket_server == -1) {
         perror("socket_serveur");
+	return -1;
     }
 
     // Attachement de la socket à une interface réseau et paramètrage
     if (bind(socket_server, (struct sockaddr *)&socketAddr, sizeof(socketAddr)) == -1) {
         perror("bind socket_serveur");
+	return -1;
     }
 
     // démarrer l'attente de connexions
     if (listen(socket_server, 10) == -1) {
         perror("listen socket_serveur");
+	return -1;
     }
+    return socket_server;
 }
-
-
